e3fa88f6af2a01c5bfececbad56e6e5a8df67824
wineewinee <wineeewineee@gmail.com>
2018-11-21 20:39:36 +0200

    New Tasks

---
+++ src/lesson7/task7.java
@@ -0,0 +1,18 @@
+package lesson7;
+//Проверяем, что все элементы равны
+public class task7 {
+    public static void main(String[] args) {
+        int [] arr = {4, 4, 4, 4};
+        System.out.println(checkEq(arr));
+    }
+
+    static boolean checkEq(int[] data){
+
+        for (int i = 0; i < data.length - 1; i++) {
+            if (data[i] != data[i + 1]){
+                    return false;
+            }
+        }
+        return true;
+    }
+}
