f9cf1ac5c5b79d522a194ad4318f2f3a942a8345
wineewinee <wineeewineee@gmail.com>
2018-12-05 20:39:58 +0200

    new tasks++

---
+++ src/lesson10/task1.java
@@ -0,0 +1,57 @@
+package lesson10;
+//Написать класс калькулятора, хранящего вещественное число x и понимающего следующие команды:
+//прибавить к этому числу значение параметра, вычесть из него, домножить его и разделить, а также извлечь из этого числа
+//квадратный корень и взять тригонометрическую функцию. Написать еще один класс, кроме перечисленного
+//имеющий одну ячейку памяти и понимающий команды записать в память, извлечь из памяти, добавить x к содержимому памяти
+
+public class task1 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Calculator{
+    double x;
+
+    public double add(double y){
+     return x + y;
+    }
+
+    public double subtract(double y){
+        return x - y;
+    }
+
+    public double multiply(double y){
+        return x * y;
+    }
+
+    public double divide(double y){
+        return x / y;
+    }
+
+    public double square(double y){
+        return Math.sqrt(y);
+    }
+
+    public double function(double y){
+        return Math.sin(y);
+    }
+
+
+}
+
+class getSet extends Calculator{
+    double y;
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double add(){
+        return x + y;
+    }
+}
