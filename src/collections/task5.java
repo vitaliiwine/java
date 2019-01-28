0cf0a7b17f7c23c54dd62139556dff3af44f98bf
wineewinee <wineeewineee@gmail.com>
2019-01-28 21:07:21 +0200

    - added ArrayList; - added names distribution - updated "Wrong Answer" condition - fixed FileWriter/Reader

--- src/collections/task5.java
+++ src/collections/task5.java
@@ -71,38 +71,4 @@ class Warrior implements Comparable<Warrior> {
     }
 }
 
-class Generator {
-    static Random random = new Random();
-    private static String[] warriorNames = {"warrior0", "warrior1", "warrior2", "warrior3",
-            "warrior4", "warrior5", "warrior6", "warrior7",
-            "warrior8", "warrior9" };
-
-    public static String getWarriorNames() {
-        return warriorNames[random.nextInt(warriorNames.length)];
-    }
-
-    public static String[] kingNames = {"Fridrich", "Alonso", "Arthur"};
-
-    public static String getKingNames(){
-        return kingNames[random.nextInt(kingNames.length)];
-    }
-
-    public static String[] nobleNames = {"Dart", "Shwart", "Greg", "Tom", "John", "Puss", "Calvin", "Frank"};
-
-    public static String getNobleNames(){
-        return nobleNames[random.nextInt(nobleNames.length)];
-    }
-
-    public static String[] knightNames = {"Brock", "Stan", "Lee", "Chen", "Austin", "Sven", "Gil", "Friez"};
-
-    public static String getKnightNames(){
-        return knightNames[random.nextInt(knightNames.length)];
-    }
-
-    public static String[] peasantNames = {"Bob", "Trev", "Igua", "Stain", "Bill", "Franc", "Jeff", "Clark"};
-
-    public static String getPeasantNames (){
-        return peasantNames[random.nextInt(peasantNames.length)];
-    }
-}
 
