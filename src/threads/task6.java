e60d369e33ff355659a69c294268226c19088b00
vitaliiwine <wineeewineee@gmail.com>
2019-02-03 11:45:34 +0200

    finished task

--- src/threads/task6.java
+++ src/threads/task6.java
@@ -4,8 +4,11 @@ import java.util.concurrent.Semaphore;
 
 public class task6 {
     public static void main(String[] args) {
-
-    }
+        Semaphore sem = new Semaphore(1);
+        CommonResource res = new CommonResource();
+        new Thread(new CountThread(res, sem, "Dark sorceress")).start();
+        new Thread(new CountThread(res, sem, "The Guardian")).start();
+        new Thread(new CountThread(res, sem, "Morty")).start(); }
 }
 
 
@@ -16,7 +19,8 @@ class CountThread implements Runnable {
 
     CountThread(CommonResource res, Semaphore sem, String name) {
         this.res = res;
-    }
+        this.sem = sem;
+        this.name = name; }
 
     @Override
     public void run() {
@@ -28,16 +32,11 @@ class CountThread implements Runnable {
             for (int i = 1; i < 5; i++) {
                 System.out.println(this.name + ": " + res.x);
                 res.x++;
-                Thread.sleep(100);
-            }
+                Thread.sleep(100); }
         } catch (InterruptedException e){
-            System.out.println(e.getMessage());
-        }
-        System.out.println(name + "frees up boiler access");
-    }
-
+            System.out.println(e.getMessage()); }
+        System.out.println(name + " frees up boiler access"); }
 }
 
 class CommonResource {
-    int x = 0;
-}
+    int x = 0; }
